e34cfcd7d20ef4ea76e3fa73cf3885a76f593d0e
Gemma Millan Zamora <millan@embl.de>
2026-08-06 16:13:15 +0200

    database implementation into UI

--- packages/limblab-gui/limblab_gui/app.py
+++ packages/limblab-gui/limblab_gui/app.py
@@ -1,7 +1,6 @@
 # pyright: reportOptionalMemberAccess=false
 from PyQt6.QtWidgets import  QApplication
 from main import MainWindow
-from limblab import print_hello
 
   # Call the function from database.py to verify import 
 # Run the application
