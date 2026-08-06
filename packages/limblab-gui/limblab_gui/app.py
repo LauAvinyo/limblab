cfa5738ac440abd843e10d9536fa593a50ee47bb
Laura <laura@itservices1s-MacBook-Pro-2.local>
2026-08-06 11:29:27 +0200

    ADD limblab on app.py

--- packages/limblab-gui/limblab_gui/app.py
+++ packages/limblab-gui/limblab_gui/app.py
@@ -1,7 +1,10 @@
 # pyright: reportOptionalMemberAccess=false
 from PyQt6.QtWidgets import  QApplication
-from .main import MainWindow
+from main import MainWindow
+from limblab import print_hello
 
+print("Starting LimbLab GUI...")
+print_hello()  # Call the function from database.py to verify import 
 # Run the application
 app = QApplication([])
 window = MainWindow()
